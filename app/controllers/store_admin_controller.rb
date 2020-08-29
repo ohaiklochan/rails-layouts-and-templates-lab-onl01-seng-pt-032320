faffd96b9c3806c555d64ac6e4a12ee24763bc5c
Chloe Steffens <caforonda103@yahoo.com>
2020-08-29 23:25:47 +0000

    Automatically backed up by Learn

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -2,9 +2,11 @@ class StoreAdminController < ApplicationController
   layout "admin"
   
   def order
+    render layout => "order_administration"
   end
 
   def invoice
+    render layout => false
   end
 
   def home
